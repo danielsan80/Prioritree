bbdcd60f61cd99e2012056d30d24784250b3093a
danielsan80 <danilo.sanchi@gmail.com>
2013-01-21 22:20:42 +0100

    porting of main script

--- web/index.php
+++ web/index.php
@@ -4,7 +4,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/', function () use ($app) {
-    return 'It works';
+    $file = __DIR__.'/../data/mytasks.yml';
+    $builder = new Dan\Prioritree\TaskBuilder();
+    $root = $builder->loadFromFile($file);
+    file_put_contents($file, $root->getAsYaml());	
+    return '<h1>done</h1>';
 });
 
 $app->run();
